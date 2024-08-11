4236e5d9dc131b60029799bfa676368669e91c87
pranav322 <pranav88096@gmail.com>
2024-08-11 12:11:01 +0530

    jesus again again

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 #!/usr/bin/env node
 
 function logDetails() {
-    const message = "Hey! I'm Pranav, A professional chatgpt prompter.";
-    const message2 = "Let's Connect and Build Now!!";
-    const twitterLink = "https://twitter.com/_pranav69";
-    const linkedinLink = "https://www.linkedin.com/in/pranawww/";
-    const instagramLink = "https://www.instagram.com/_pranawww/";
-    const githubLink = "https://www.github.com/Pranav322";
-
-    // ASCII Art of PRANAWWW
     const asciiArt = `
     ██████╗ ██████╗  █████╗ ███╗   ██╗ █████╗ ██╗    ██╗██╗    ██╗██╗    ██╗
     ██╔══██╗██╔══██╗██╔══██╗████╗  ██║██╔══██╗██║    ██║██║    ██║██║    ██║
@@ -18,20 +10,34 @@ function logDetails() {
     ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚══╝╚══╝  ╚══╝╚══╝  ╚══╝╚══╝
     `;
 
+    const details = `
+    \x1b[1m\x1b[36mName:\x1b[0m Pranawww
+    \x1b[1m\x1b[33mAbout:\x1b[0m A Professional chatgpt prompter
+    \x1b[1m\x1b[33mWebsite:\x1b[0m https://pranaw.vercel.app
+    \x1b[1m\x1b[35mGitHub:\x1b[0m https://github.com/Pranav322
+    \x1b[1m\x1b[31mTwitter:\x1b[0m https://twitter.com/_pranav69
+    \x1b[1m\x1b[36mEmail:\x1b[0m duckieduckk@duck.com
+
+    \x1b[1m\x1b[33mLanguages:\x1b[0m
+    \x1b[33mJavaScript\x1b[0m \x1b[34mPython\x1b[0m \x1b[32mDart\x1b[0m \x1b[34mGo\x1b[0m \x1b[31mJava\x1b[0m
+
+    \x1b[1m\x1b[33mFrameworks:\x1b[0m
+    \x1b[31mNextJs\x1b[0m \x1b[34mExpressJs\x1b[0m \x1b[32mTailwindCss\x1b[0m \x1b[34mDjango\x1b[0m \x1b[31mReact\x1b[0m \x1b[90mMaterial UI\x1b[0m
+
+    \x1b[1m\x1b[33mOthers:\x1b[0m
+    \x1b[36mDocker\x1b[0m \x1b[32mMongodb\x1b[0m \x1b[90mPostgreSQL\x1b[0m \x1b[31mGit\x1b[0m \x1b[36mLinux\x1b[0m
+    `;
+
     // Create a colorful box using ANSI escape codes
     const colorfulBox = `
     \x1b[38;5;51m+-----------------------------------------------------------------+
-    |   \x1b[38;5;105m${message}      \x1b[38;5;51m|
-    |   \x1b[38;5;105m${message2}                                 \x1b[38;5;51m|
-    +-----------------------------------------------------------------+\x1b[0m
-    | \x1b[38;5;93mTwitter:\x1b[0m \x1b[38;5;39m${twitterLink}                      \x1b[38;5;51m|
-    | \x1b[38;5;93mLinkedIn:\x1b[0m \x1b[38;5;39m${linkedinLink}  \x1b[38;5;51m|
-    | \x1b[38;5;93mInstagram:\x1b[0m \x1b[38;5;39m${instagramLink}                 \x1b[38;5;51m|
-    | \x1b[38;5;93mGitHub:\x1b[0m \x1b[38;5;39m${githubLink}                    \x1b[38;5;51m|
+    ${asciiArt}
+    +-----------------------------------------------------------------+
+    ${details.split('\n').map(line => '    |' + line).join('\n')}
     +-----------------------------------------------------------------+\x1b[0m`;
 
-    // Log the ASCII art followed by the colorful box in the terminal
-    console.log(asciiArt + colorfulBox);
+    // Log the colorful box with ASCII art and details in the terminal
+    console.log(colorfulBox);
 }
 
 // Call the function to log your details
